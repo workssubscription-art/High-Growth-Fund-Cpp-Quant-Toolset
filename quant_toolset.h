df03c3dcfab3a198684974b35d000218a798ed07
workssubscription-art <workssubscription@gmail.com>
2026-05-04 04:38:54 -0400

    Add toolset stub

---
+++ quant_toolset.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <algorithm>
+#include <charconv>
+#include <cctype>
+#include <deque>
+#include <filesystem>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <vector>
